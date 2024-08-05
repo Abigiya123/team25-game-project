7b6ea6ada4b926013f04ec2be5fe92906ffcdc82
Anh Ngo <qanhngo.cs@gmail.com>
2024-08-06 06:51:00 +0700

    initializes pygame, screen, clock, set display window's caption, and call main()

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import pygame
+
+def main():
+    pygame.init()
+    pygame.display.set_caption("Austin v.s. Felipe")
+    screen = pygame.display.set_mode((1500, 1000))
+    clock = pygame.time.Clock()
+
+main()
