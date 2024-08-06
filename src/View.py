8bf2029095fb283c99932b7b1fcf02dbe105d77e
Anh Ngo <qanhngo.cs@gmail.com>
2024-08-06 07:06:32 +0700

    basic structure for View class

---
+++ src/View.py
@@ -0,0 +1,15 @@
+import pygame
+from Game import Game
+
+class View:
+    """Constructor"""
+    def __init__(self, screen: pygame.Surface, game: Game) -> None:
+        pass
+    
+    """
+    - Draws the background
+    - Asks the Game to draw all of its objects
+    """
+    def draw_everything() -> None:
+        pass
+    
